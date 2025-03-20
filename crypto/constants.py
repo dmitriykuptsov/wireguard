1e4f0abedf190815810f8cdc069713ea0b58d276
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-03-20 16:36:09 +0500

    Added constants

---
+++ crypto/constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+# Copyright (C) 2019 strangebit
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+CONSTRUCTION = "Noise_IKpsk2_25519_ChaChaPoly_BLAKE2s"
+IDENTIFIER = "WireGuard v1 zx2c4 Jason@zx2c4.com"
+LABEL_MAC1 = "mac1----"
+LABEL_COOKIE = "cookie--"
