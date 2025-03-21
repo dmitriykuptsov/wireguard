13903aa2851bea71174312a6ee98de40d9a75506
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-03-21 23:32:52 +0500

    Added routing table ops

--- routing/cryptoroute.py
+++ routing/cryptoroute.py
@@ -1,3 +1,5 @@
+from base64 import b64decode, b64encode
+from utils.misc import Math
 class CryptoRoutingEntry():
 
     ip     = None
@@ -13,8 +15,14 @@ class CryptoRoutingEntry():
     NRecv  = 0
     Cr     = None
     Ci     = None
+    Hr     = None
+    Hi     = None
+    Epriv  = None
+    Epub   = None
     state  = None
     rekey_timeout = 0
+    cookie = "".encode("UTF-8")
+    cookie_timeout = 0
 
     def __init__(self, ip_s, ip, prefix, key, port):
         self.ip_s = ip_s
@@ -22,11 +30,25 @@ class CryptoRoutingEntry():
         self.prefix = prefix
         self.key = key
         self.port = port
-    def match(self, dst):
+    
+    def match_by_ip(self, dst):
         dst = dst & self.prefix
         if dst & self.ip == self.ip:
             return True
         return False
+    def match_by_key(self, key):
+        if key == self.key:
+            return True
+        return False
+    def match_by_id(self, id):
+        if id == self.I:
+            return True
+        return False
+    
+    def __str__(self):
+        prefix = Math.int_to_bytes(self.prefix)
+        prefix = prefix[0] + "." + prefix[1] + "." + prefix[2] + "." + prefix[3]
+        return b64encode(self.key) + " " + self.ip_s + " " + prefix
 
 def recmp(left, right):
     if left.ip < right.ip:
@@ -40,15 +62,26 @@ class RoutingTable():
         self.table = []
     def add(self, entry):
         self.table.append(entry)
-        self.table.sort(recmp)
     def delete(self, entry):
         for e in self.table:
             if e.key == entry.key:
                 self.table.remove(e)
                 break;
-    def get(self, ip):
+    
+    def get_by_ip(self, ip):
         for e in self.table:
-            if e.match(ip):
+            if e.match_by_ip(ip):
                 return e
         return None
     
+    def get_by_key(self, key):
+        for e in self.table:
+            if e.match_by_key(key):
+                return e
+        return None
+    
+    def get_by_id(self, id):
+        for e in self.table:
+            if e.match_by_id(id):
+                return e
+        return None
