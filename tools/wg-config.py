dac54a0bdf6b34fd731f6131e8cceaa0ce556c7c
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2025-03-24 15:12:45 +0500

    Testing access control

---
+++ tools/wg-config.py
@@ -0,0 +1,37 @@
+# Sockets
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connec(('127.0.0.1', 10000))
+
+commands = {
+    "list": [
+        "routes"
+    ],
+    "add": {
+        "route": ["<destination ip> <prefix> <public key base 64 encoded> <peer> <port>"]
+    },
+    "status": []
+}
+
+reading = True
+
+while reading:
+    print('Enter command to send to wg server:')
+    s = None
+    command = ""
+    while True:
+        data = input()
+        if data == '\n':
+            s.send(command.encode('ASCII'))
+            break
+        if data == '\t':
+            subcommands = command.split(" ")
+            last = subcommands[-1]
+            if s:
+                s = commands[last]
+            
+            print("")
+        if data == "exit":
+            reading = False
+            break
